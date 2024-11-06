e551e16be8480b7839134014fd0822467b877a82
Lee9747 <leejunho32@naver.com>
2024-11-07 01:06:27 +0900

    CTile 구현(아직 미완성)

--- Engine/Include/GameObject.h
+++ Engine/Include/GameObject.h
@@ -37,7 +37,7 @@ public:
 	void AddPos(const Vector2& pos) { m_pos.x += pos.x; m_pos.y += pos.y; }
 	void AddAngle(float angle) { m_angle += angle; }
 
-	void SetTexture(const std::string& name, const std::wstring& fileName, EObject_Dir dir, ETexture_Type type = ETexture_Type::Sprite, const std::string& pathName = TEXTURE_PATH);
+	virtual void SetTexture(const std::string& name, const std::wstring& fileName, EObject_Dir dir, ETexture_Type type = ETexture_Type::Sprite, const std::string& pathName = TEXTURE_PATH);
 	void SetAnimation(RECT* rects, int num, EObject_State state = EObject_State::Basic, ETexture_Type type = ETexture_Type::CIMAGE);
 	bool SetColorKey(unsigned char r, unsigned char g, unsigned char b, int idx = 0);
 
